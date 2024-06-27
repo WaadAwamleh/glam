98bda2f6561202879ae51861d56dc03e204f324f
Waad Awamleh <danaawamleh@Airon.local>
2024-06-28 00:45:49 +0200

    all files

---
+++ GLAMpoints_pytorch-master/training_glam_detector.py
@@ -0,0 +1,243 @@
+import numpy as np
+from tqdm import tqdm
+import pickle
+import random
+import json
+from datetime import datetime
+from pathlib import Path
+from termcolor import colored
+import os
+import os.path as osp
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.multiprocessing as mp
+from torch.utils.data import DataLoader
+from utils.dataset import SyntheticDataset
+from utils.utils import check_and_reshape, plot_losses, plot_training
+from utils.loss import compute_reward, compute_loss
+from utils.logger import LoggerUtility
+from utils.checkpoint_manager import get_most_recent_checkpoint,load_checkpoint, save_checkpoint
+#from utils.unet import UNet
+from utils.glam import UNet
+
+
+
+def main(con: dict, args) -> None:
+
+    n_jobs = con['n_jobs']
+    dataset = con['dataset']
+    batch_size = con['batch_size']
+    lr = con['learning_rate']
+    weight_decay = con['weight_decay']
+    epochs = con['epochs']
+    verbose = con['compute_metrics']
+    
+    logger = LoggerUtility.get_logger()
+    cwd = Path.cwd()
+    model_path = cwd.joinpath('model')
+    if not model_path.exists():
+        model_path.mkdir(mode=0o770, parents=True, exist_ok=True)
+    
+    random.seed(2021)
+    np.random.seed(2021)
+    torch.manual_seed(2021)
+    torch.cuda.manual_seed(2021)
+    date = datetime.now()
+    name = date.strftime('%Y%m%d')
+    train_losses = []
+    val_losses = []
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train_dataset = SyntheticDataset(Path(dataset) , train=True)
+    val_dataset = SyntheticDataset(Path(dataset) , train=False)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=n_jobs)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=n_jobs)
+    model = UNet()
+    optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()),lr=lr,weight_decay=weight_decay)
+    save_path = Path(con['write_dir']) / con['name_exp']
+    if con["load_pretrained_weights"]:
+        checkpoints = Path(con["pretrained_weights"])
+        if not checkpoints.exists():
+            logger.error('The path to the model pre-trained weights you indicated does not exist.')
+        else:
+            filename = get_most_recent_checkpoint(checkpoints)
+            logger.info('path to pretrained model is {}'.format(checkpoints))      
+            model, optimizer, start_epoch, best_val = load_checkpoint(model, optimizer, filename=filename)
+            for state in optimizer.state.values():
+                for k, v in state.items():
+                    if isinstance(v, torch.Tensor):
+                        state[k] = v.to(device)       
+    else:
+        if not save_path.exists():
+            save_path.mkdir(parents=True, exist_ok=True)
+        with open(save_path / 'name.pkl', 'wb') as f: #change it to f'{name}.pkl'
+            pickle.dump(name, f)  
+        best_val = -1
+        start_epoch = 0   
+    model = nn.DataParallel(model)
+    model = model.to(device)
+    save_train = Path(save_path) / 'train'
+    save_train .mkdir(parents=True,exist_ok=True)
+    save_valid = Path(save_path) / 'valid'
+    save_valid .mkdir(parents=True,exist_ok=True)
+    for epoch in range(start_epoch, epochs):
+        print('starting epoch {}'.format(epoch))
+        if epoch == 0:
+            nms = con['nms0']
+        else:
+            nms = con['nms']   
+        n_iter = epoch*len(train_dataloader)
+        model.train()
+        running_train_loss = 0
+        if verbose:
+            nbr_images = 0.0
+            nbr_homo_correct = 0.0
+            nbr_homo_accept = 0.0
+            nbr_tp = 0.0
+            nbr_kp = 0.0
+            add_repeatability = 0.0
+            
+        pbar = tqdm(enumerate(train_dataloader), total=len(train_dataloader))
+        for i, mini_batch in pbar:
+            image1 = check_and_reshape(mini_batch['image1'].to(device))
+            image2 = check_and_reshape(mini_batch['image2'].to(device))
+            image1_normed = mini_batch['image1_normed'].to(device)
+            image2_normed = mini_batch['image2_normed'].to(device)
+            homographies = mini_batch['H1_to_2']
+            optimizer.zero_grad()           
+            kp_map1 = model(check_and_reshape(image1_normed)) # shape Bx1xHxW
+            kp_map2 = model(check_and_reshape(image2_normed))
+            computed_reward1, mask_batch1, metrics_per_image = compute_reward(image1, 
+                                                                              image2, 
+                                                                              kp_map1.clone().detach(),
+                                                                              kp_map2.clone().detach(),
+                                                                              homographies, 
+                                                                              nms,
+                                                                              distance_threshold=con['distance_threshold'],
+                                                                              device=device,
+                                                                              compute_metrics=verbose)
+            
+            if verbose:
+                nbr_images += metrics_per_image['nbr_images']
+                nbr_homo_correct += metrics_per_image['nbr_homo_correct']
+                nbr_homo_accept += metrics_per_image['nbr_homo_acceptable']
+                nbr_tp += metrics_per_image['nbr_tp']
+                nbr_kp += metrics_per_image['nbr_kp']
+                add_repeatability += metrics_per_image['sum_rep']
+                logger.info(f'cumulative_ratio_correct_homographies_per_iter, {float(nbr_homo_correct) / nbr_images}, {n_iter}')
+                logger.info(f'cumulative_ratio_acceptable_homographies_per_iter {float(nbr_homo_accept) / nbr_images}, {n_iter}')
+                logger.info(f'cumulative_average_number_of_kp_per_iter {float(nbr_kp) / nbr_images}, {n_iter}')
+                logger.info(f'cumulative_average_number_of_tp_per_iter {float(nbr_tp) / nbr_images}, {n_iter}')
+                logger.info(f'cumulative_repeatability_per_iter, {float(add_repeatability) / nbr_images}, {n_iter}')
+            Loss = compute_loss(reward=computed_reward1, kpmap=kp_map1, mask=mask_batch1)
+            Loss.backward()
+            optimizer.step()
+            train_losses.append(Loss.item())
+            if i % con["plot_every_x_batches"] == 0 and verbose:
+                plot_training(image1.cpu().numpy().squeeze(1), image2.cpu().numpy().squeeze(1),
+                            kp_map1.detach().cpu().numpy().squeeze(1), kp_map2.detach().cpu().numpy().squeeze(1),
+                            computed_reward1.cpu().numpy().squeeze(1), Loss.item(),
+                            mask_batch1.cpu().numpy().squeeze(1), metrics_per_image, epoch, save_train,
+                            name_to_save='epoch{}_batch{}.jpg'.format(epoch, i))
+            running_train_loss += Loss.item()
+            logger.info('train_loss_per_iter', Loss.item(), n_iter)
+            n_iter += 1
+            pbar.set_description('training: R_total_loss: %.3f/%.3f' % (running_train_loss / (i + 1),Loss.item()))
+            
+        running_train_loss /= len(train_dataloader)
+        
+        logger.info('train loss', running_train_loss, epoch)
+        print(colored('==> ', 'green') + 'Train average loss:', running_train_loss)
+        if con['validation']:
+            n_iter = epoch * len(val_dataloader)
+            model.eval()
+            running_valid_loss = 0
+            if verbose:
+                nbr_images = 0.0
+                nbr_homo_correct = 0.0
+                nbr_homo_accept = 0.0
+                nbr_tp = 0.0
+                nbr_kp = 0.0
+                add_repeatability = 0.0
+            with torch.no_grad():
+                pbar = tqdm(enumerate(val_dataloader), total=len(val_dataloader))
+                for i, mini_batch in pbar:
+                    image1 = mini_batch['image1'].to(device)
+                    image2 = mini_batch['image2'].to(device)
+                    image1_normed = mini_batch['image1_normed'].to(device)
+                    image2_normed = mini_batch['image2_normed'].to(device)
+                    homographies = mini_batch['H1_to_2']
+                    kp_map2 = model(image2_normed)
+                    kp_map1 = model(image1_normed)
+                    computed_reward1, mask_batch1, metrics_per_image = compute_reward(image1,
+                                                                                      image2,
+                                                                                      kp_map1.clone(),
+                                                                                      kp_map2.clone(),
+                                                                                      homographies, 
+                                                                                      nms,
+                                                                                      distance_threshold=con['distance_threshold'],
+                                                                                      device=device,
+                                                                                      compute_metrics=verbose)
+                    if verbose:
+                        nbr_images += metrics_per_image['nbr_images']
+                        nbr_homo_correct += metrics_per_image['nbr_homo_correct']
+                        nbr_homo_accept += metrics_per_image['nbr_homo_acceptable']
+                        nbr_tp += metrics_per_image['nbr_tp']
+                        nbr_kp += metrics_per_image['nbr_kp']
+                        add_repeatability += metrics_per_image['sum_rep']
+                        logger.info(f'cumulative_ratio_correct_homographies_per_iter, {float(nbr_homo_correct)/nbr_images}, {n_iter}')
+                        logger.info(f'cumulative_ratio_acceptable_homographies_per_iter, {float(nbr_homo_accept)/nbr_images}, {n_iter}')
+                        logger.info(f'cumulative_average_number_of_kp_per_iter, {float(nbr_kp)/nbr_images}, {n_iter}')
+                        logger.info(f'cumulative_average_number_of_tp_per_iter, {float(nbr_tp)/nbr_images}, {n_iter}')
+                        logger.info(f'cumulative_repeatability_per_iter, {float(add_repeatability)/nbr_images}, {n_iter}')
+                    Loss = compute_loss(reward=computed_reward1, kpmap=kp_map1, mask=mask_batch1)
+                    val_losses.append(Loss.item())
+                    if i < 2 and verbose:
+                        plot_training(image1.cpu().numpy().squeeze(), image2.cpu().numpy().squeeze(),
+                                    kp_map1.cpu().numpy().squeeze(), kp_map2.cpu().numpy().squeeze(),
+                                    computed_reward1.cpu().numpy().squeeze(), 
+                                    Loss.item(),
+                                    mask_batch1.cpu().numpy().squeeze(), 
+                                    metrics_per_image, epoch, save_valid,
+                                    name_to_save='epoch{}_batch{}'.format(epoch, i))
+                    running_valid_loss += Loss.item()
+                    logger.info('val_loss_per_iter', Loss.item(), n_iter)
+                    n_iter += 1
+                    pbar.set_description('validation: R_total_loss: %.3f/%.3f' % (running_valid_loss / (i + 1),Loss.item()))
+                running_valid_loss /= len(val_dataloader)
+                
+                if verbose:
+                    logger.info('ratio_correct_homographies_per_epoch',float(nbr_homo_correct) / nbr_images, epoch)
+                    logger.info('ratio_acceptable_homographies_per_epoch',float(nbr_homo_accept) / nbr_images, epoch)
+                    logger.info('average_number_of_kp_per_epoch', float(nbr_kp) / nbr_images, epoch)
+                    logger.info('average_number_of_tp_per_epoch', float(nbr_tp) / nbr_images, epoch)
+                    logger.info('repeatability_per_epoch', float(add_repeatability) / nbr_images, epoch)
+            val_loss =  running_valid_loss / len(val_dataloader)
+            print(colored('==> ', 'blue') + 'Val average grid loss :', val_loss)
+            print(colored('==> ', 'blue') + 'epoch :', epoch + 1)
+            logger.info('val loss', val_loss, epoch)
+            if best_val < 0:
+                best_val = val_loss
+            is_best = val_loss < best_val
+            best_val = min(val_loss, best_val)
+        else:
+            is_best = False
+        save_checkpoint({'epoch': epoch + 1,'state_dict': model.module.state_dict(),'optimizer': optimizer.state_dict(),
+                         'best_loss': best_val},is_best, save_valid, 'epoch_{}.pth'.format(epoch + 1))
+    plot_losses(train_losses, val_losses)
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    import json
+    local_config = Path(__file__).resolve().parent / "configs"
+    local_config.mkdir(parents=True, exist_ok=True)
+    local_config = Path(local_config / "train.json")
+    parser = ArgumentParser(description="process and configure dataset handling")
+    parser.add_argument("--data_path",dest="data_path",default=None,help="Path to overwrite data input from config file")
+    args = parser.parse_args()
+    LoggerUtility.setup_logging(local_config)
+    with open(local_config, "r", encoding='utf-8') as config_file:
+        config = json.load(config_file)
+    if args.data_path:
+        config['dataset'] = args.data_path
+    mp.set_start_method('spawn')  # or 'fork' if you are on a Unix-like system
+    main(config, args)
---
+++ GLAMpoints_pytorch-master/utils/utils.py
@@ -0,0 +1,87 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+from pathlib import Path
+
+def check_and_reshape(img):
+    if len(img.shape) == 2: 
+        img = img.unsqueeze(0)  
+    return img
+
+def plot_losses(train_losses, val_losses):
+    plt.figure(figsize=(10, 5))
+    plt.plot(train_losses, label='Training Loss')
+    plt.plot(val_losses, label='Validation Loss')
+    plt.title('Training and Validation Losses')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig('/Users/danaawamleh/Desktop/JKU/Practical/training_validation_loss.png')
+    plt.show()
+
+def plot_training(image1, image2, kp_map1, kp_map2, computed_reward1, loss, mask_batch1, metrics_per_image,
+                  epoch, save_path, name_to_save):
+
+    fig, ((axis1, axis2), (axis3, axis4), (axis5, axis6), (axis7, axis8)) = \
+        plt.subplots(4, 2, figsize=(20, 20))
+    nbr=0
+    size_image = image1[nbr, :, :].shape
+    plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.0002, hspace=0.4)
+    axis1.imshow(image1[nbr, :, :], cmap='gray', vmin=0, vmax=255)
+    axis1.scatter(metrics_per_image['{}'.format(nbr)]['to_plot']['tp_kp1'][1],
+                  metrics_per_image['{}'.format(nbr)]['to_plot']['tp_kp1'][0], s=4, color='red')
+    axis1.set_title('epoch{}, img1:original_image, \n true positive points in red'.format(epoch), fontsize='small')
+
+    im = axis2.imshow(image2[nbr, :, :], cmap='gray', vmin=0, vmax=255)
+    axis2.scatter(metrics_per_image['{}'.format(nbr)]['to_plot']['warped_tp_kp1'][1],
+                  metrics_per_image['{}'.format(nbr)]['to_plot']['warped_tp_kp1'][0], s=2,
+                  color='red')
+    axis2.set_title('img2:warped_image', fontsize='small')
+    fig.colorbar(im, ax=axis2)
+    axis3.imshow(kp_map1[nbr, :, :], vmin=0.0, vmax=1.0, interpolation='nearest')
+    axis3.set_title('kp map of image1, max {}, min {}'.format(np.max(kp_map1[nbr]),
+                                                              np.min(kp_map1[nbr])),
+                    fontsize='small')
+    im4 = axis4.imshow(kp_map2[nbr, :, :], vmin=0.0, vmax=1.0, interpolation='nearest')
+    axis4.set_title('kp map of image2', fontsize='small')
+    fig.colorbar(im4, ax=axis4)
+    axis5.imshow(image1[nbr, :, :], cmap='gray', origin='upper', vmin=0.0, vmax=255.0)
+    axis5.scatter(metrics_per_image['{}'.format(nbr)]['to_plot']['keypoints_map1'][1],
+                  metrics_per_image['{}'.format(nbr)]['to_plot']['keypoints_map1'][0], s=2,
+                  color='green')
+    axis5.scatter(metrics_per_image['{}'.format(nbr)]['to_plot']['tp_kp1'][1],
+                  metrics_per_image['{}'.format(nbr)]['to_plot']['tp_kp1'][0], s=4, color='red')
+    axis5.set_title('kp_map after NMS of image1 in green, nbr_kp {}, \n nbr tp keypoints found {} (in red)'.format(
+        metrics_per_image['{}'.format(nbr)]['nbr_kp1'],
+        metrics_per_image['{}'.format(nbr)]['total_nbr_kp_reward1']), fontsize='medium')
+
+    im6 = axis6.imshow(image2[nbr, :, :], cmap='gray', origin='upper', vmin=0.0, vmax=255.0)
+    axis6.scatter(metrics_per_image['{}'.format(nbr)]['to_plot']['keypoints_map2'][1],
+                  metrics_per_image['{}'.format(nbr)]['to_plot']['keypoints_map2'][0], s=2,
+                  color='green')
+    axis6.scatter(metrics_per_image['{}'.format(nbr)]['to_plot']['warped_tp_kp1'][1],
+                  metrics_per_image['{}'.format(nbr)]['to_plot']['warped_tp_kp1'][0], s=4,
+                  color='red')
+    axis6.set_title('kp_map after NMS of image2 in green, nbr_kp {} \n true positive points in red'.format(
+        metrics_per_image['{}'.format(nbr)]['nbr_kp2']), fontsize='small')
+    fig.colorbar(im6, ax=axis6)
+
+    axis7.imshow(computed_reward1[nbr, :, :], vmin=0, vmax=1, interpolation='nearest')
+    axis7.set_title('computed reward:,\nrepeatability={}, total_nbr_tp_kp={} \n binary mask for backpropagation sum={}'.format(
+        metrics_per_image['{}'.format(nbr)]['repeatability'],
+        metrics_per_image['{}'.format(nbr)]['total_nbr_kp_reward1'],
+    np.sum(mask_batch1[nbr, :, :])), fontsize='small')
+
+    im8 = axis8.imshow(
+        cv2.warpPerspective(image1[nbr, :, :], metrics_per_image['{}'.format(nbr)]
+        ['computed_H'], (size_image[1], size_image[0])), cmap='gray', vmin=0, vmax=255)
+    axis8.set_title('warped image 1 according to computed homography,\n inlier_ratio={}, '
+                     'true_homo={}, class_acceptable={}'.format(
+        metrics_per_image['{}'.format(nbr)]['inlier_ratio'],
+        metrics_per_image['{}'.format(nbr)]['homography_correct'],
+        metrics_per_image['{}'.format(nbr)]['class_acceptable']), fontsize='small')
+    fig.colorbar(im8, ax=axis8)
+    fig.savefig(Path(save_path) / (name_to_save), bbox_inches='tight')
+    plt.close(fig)
+
